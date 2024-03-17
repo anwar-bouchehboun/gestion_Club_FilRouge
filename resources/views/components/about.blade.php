df39e7ec2646c7a90d9bf1376be074ca8e60695b
anouar <anouar.ab95@gmail.com>
2024-03-17 21:24:21 +0000

    update dgsin color

--- resources/views/components/about.blade.php
+++ resources/views/components/about.blade.php
@@ -1,49 +1,43 @@
-<div class="max-w-screen-xl mx-auto py-8 px-4 lg:py-16 lg:px-6">
-    <div class=" mb-10">
-        <h2 class="uppercase text-4xl font-bold md:ms-12 ms-3">About us</h2>
+<div class="max-w-screen-xl px-4 py-8 mx-auto lg:py-16 lg:px-6">
+    <div class="mb-10 ">
+        <h2 class="text-4xl font-bold uppercase md:ms-12 ms-3">About us</h2>
     </div>
-
     <div class="flex flex-col md:flex-row">
         <!-- can help image -->
-        <div class="mr-0 md:mr-8 mb-6 md:mb-0">
-            <img class="w-1/2 md:w-full mx-auto" src="./storage/image/logo.png" alt="can_help_banner">
+        <div class="mb-6 mr-0 md:mr-8 md:mb-0">
+            <img class="mx-auto md:w-full w-96" src="./storage/image/logo.png" alt="can_help_banner">
         </div>
         <!-- end can help image -->
 
-        <div class="flex-1 flex flex-col sm:flex-row flex-wrap -mb-4 -mx-2">
-            <div class="w-full sm:w-1/2 mb-4 px-2 ">
+        <div class="flex flex-col flex-wrap flex-1 -mx-2 -mb-4 md:flex-row">
+            <div class="w-full px-2 mb-4 md:w-1/2">
                 <svg class="w-[47px] h-[47px] text-gray-800 dark:text-white" aria-hidden="true"
                     xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
                     <path stroke="currentColor" stroke-linecap="round" stroke-width="1.5"
                         d="M4.37 7.657c2.063.528 2.396 2.806 3.202 3.87 1.07 1.413 2.075 1.228 3.192 2.644 1.805 2.289 1.312 5.705 1.312 6.705M20 15h-1a4 4 0 0 0-4 4v1M8.587 3.992c0 .822.112 1.886 1.515 2.58 1.402.693 2.918.351 2.918 2.334 0 .276 0 2.008 1.972 2.008 2.026.031 2.026-1.678 2.026-2.008 0-.65.527-.9 1.177-.9H20M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
                 </svg>
-                <div class="h-full py-4 px-6 ">
-
-
-
-                    <h3 class="text-2xl font-bold text-md mb-6 text-[#24B49A]"> Social Clubs:</h3>
+                <div class="h-full px-6 py-4">
+                    <h3 class="text-2xl font-bold text-md mb-6 text-[#24B49A]"> Social Clubs</h3>
                     <p class="text-sm">These clubs are formed for the primary purpose of socializing and building
                         friendships. They may organize social events, outings, and gatherings for members to connect and
                         interact.</p>
                 </div>
             </div>
-            <div class="w-full sm:w-1/2 mb-4 px-2 ">
+            <div class="w-full px-2 mb-4 md:w-1/2">
                 <svg class="w-[47px] h-[47px] text-gray-800 dark:text-white" aria-hidden="true"
                     xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor"
                     viewBox="0 0 24 24">
                     <path
                         d="M20 7h-.7c.229-.467.349-.98.351-1.5a3.5 3.5 0 0 0-3.5-3.5c-1.717 0-3.215 1.2-4.331 2.481C10.4 2.842 8.949 2 7.5 2A3.5 3.5 0 0 0 4 5.5c.003.52.123 1.033.351 1.5H4a2 2 0 0 0-2 2v2a1 1 0 0 0 1 1h18a1 1 0 0 0 1-1V9a2 2 0 0 0-2-2Zm-9.942 0H7.5a1.5 1.5 0 0 1 0-3c.9 0 2 .754 3.092 2.122-.219.337-.392.635-.534.878Zm6.1 0h-3.742c.933-1.368 2.371-3 3.739-3a1.5 1.5 0 0 1 0 3h.003ZM13 14h-2v8h2v-8Zm-4 0H4v6a2 2 0 0 0 2 2h3v-8Zm6 0v8h3a2 2 0 0 0 2-2v-6h-5Z" />
                 </svg>
-
-                <div class="h-full py-4 px-6 ">
+                <div class="h-full px-6 py-4">
                     <h3 class="text-2xl font-bold text-md mb-6 text-[#24B49A]">Hobby Clubs</h3>
                     <p class="text-sm"> Hobby clubs are centered around shared interests or hobbies, such as
                         photography, gardening, cooking, or gaming. Members come together to pursue their passions,
                         share knowledge and skills, and engage in related activities.</p>
                 </div>
             </div>
-
-            <div class="w-full sm:w-1/2 mb-4 px-2 ">
+            <div class="w-full px-2 mb-4 md:w-1/2">
                 <svg class="w-[47px] h-[47px] text-gray-800 dark:text-white" aria-hidden="true"
                     xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor"
                     viewBox="0 0 24 24">
@@ -51,31 +45,28 @@
                         d="M12 6a3.5 3.5 0 1 0 0 7 3.5 3.5 0 0 0 0-7Zm-1.5 8a4 4 0 0 0-4 4 2 2 0 0 0 2 2h7a2 2 0 0 0 2-2 4 4 0 0 0-4-4h-3Zm6.82-3.096a5.51 5.51 0 0 0-2.797-6.293 3.5 3.5 0 1 1 2.796 6.292ZM19.5 18h.5a2 2 0 0 0 2-2 4 4 0 0 0-4-4h-1.1a5.503 5.503 0 0 1-.471.762A5.998 5.998 0 0 1 19.5 18ZM4 7.5a3.5 3.5 0 0 1 5.477-2.889 5.5 5.5 0 0 0-2.796 6.293A3.501 3.501 0 0 1 4 7.5ZM7.1 12H6a4 4 0 0 0-4 4 2 2 0 0 0 2 2h.5a5.998 5.998 0 0 1 3.071-5.238A5.505 5.505 0 0 1 7.1 12Z"
                         clip-rule="evenodd" />
                 </svg>
-
-                <div class="h-full py-4 px-6">
+                <div class="h-full px-6 py-4">
                     <h3 class="text-2xl font-bold text-md mb-6 text-[#24B49A]">Sports Clubs</h3>
                     <p class="text-sm">Sports clubs are dedicated to specific sports or athletic activities, such as
                         soccer, tennis, swimming, or basketball. Members typically participate in training sessions,
                         competitions, and events related to their chosen sport.</p>
                 </div>
             </div>
-
-            <div class="w-full sm:w-1/2 mb-4 px-2 ">
+            <div class="w-full px-2 mb-4 md:w-1/2">
                 <svg class="w-[47px] h-[47px] text-gray-800 dark:text-white" aria-hidden="true"
                     xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
                     <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                         d="M12 6.03v13m0-13c-2.819-.831-4.715-1.076-8.029-1.023A.99.99 0 0 0 3 6v11c0 .563.466 1.014 1.03 1.007 3.122-.043 5.018.212 7.97 1.023m0-13c2.819-.831 4.715-1.076 8.029-1.023A.99.99 0 0 1 21 6v11c0 .563-.466 1.014-1.03 1.007-3.122-.043-5.018.212-7.97 1.023" />
                 </svg>
-
-                <div class="h-full py-4 px-6 ">
-                    <h3 class="text-2xl font-bold text-md mb-6 text-[#24B49A]">SCultural Clubs</h3>
+                <div class="h-full px-6 py-4">
+                    <h3 class="text-2xl font-bold text-md mb-6 text-[#24B49A]">Cultural Clubs</h3>
                     <p class="text-sm">Cultural clubs celebrate and promote a particular culture, ethnicity, or
                         heritage. They may organize cultural festivals, performances, language classes, and cultural
                         exchange programs to educate and engage members and the community.</p>
                 </div>
             </div>
-
-
         </div>
     </div>
+
+
 </div>
--- resources/views/components/nav.blade.php
+++ resources/views/components/nav.blade.php
@@ -1,10 +1,10 @@
 <nav class="bg-[#000] border-gray-200 dark:bg-gray-900">
-    <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+    <div class="flex flex-wrap items-center justify-between max-w-screen-xl p-4 mx-auto">
         <a href="https://flowbite.com/" class="flex items-center space-x-3 rtl:space-x-reverse">
-            <span class="self-center text-2xl font-bold whitespace-nowrap text-white">PHONIXCLUB</span>
+            <span class="self-center text-2xl font-bold text-white whitespace-nowrap">PHONIXCLUB</span>
         </a>
         <button data-collapse-toggle="navbar-default" type="button"
-            class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-white rounded-lg md:hidden  focus:outline-none focus:ring-2 "
+            class="inline-flex items-center justify-center w-10 h-10 p-2 text-sm text-white rounded-lg md:hidden focus:outline-none focus:ring-2 "
             aria-controls="navbar-default" aria-expanded="false">
             <span class="sr-only">Open main menu</span>
             <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
@@ -14,7 +14,7 @@
             </svg>
         </button>
         <div class="hidden w-full md:block md:w-auto" id="navbar-default">
-            <ul class="font-medium flex flex-col p-4 md:p-0 mt-4 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0">
+            <ul class="flex flex-col p-4 mt-4 font-medium md:p-0 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0">
                 <li>
                     <a href="#"
                         class="text-center block uppercase py-2 px-3 text-[#24B49A]  rounded md:bg-transparent md:text-[#24B49A] md:p-1 dark:text-white md:dark:text-[#24B49A] my-2"
@@ -27,11 +27,11 @@
                 </li>
                 <li>
                     <a href="#" type="button"
-                        class=" rounded text-center sm:mb-2 block uppercase focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium text-sm px-6 py-1.5 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800 md:mx-2  my-2">Login</a>
+                        class=" rounded text-center sm:mb-2 block uppercase focus:outline-none text-white bg-[#24B49A] hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium text-sm px-6 py-1.5 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800 md:mx-2  my-2">Login</a>
                 </li>
                 <li>
                     <a href="#" type="button"
-                        class="rounded  text-center sm:mb-2 block uppercase focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium text-sm px-6 py-1.5 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800 md:mx-2  my-2">Sign
+                        class="rounded  text-center sm:mb-2 block uppercase focus:outline-none text-white bg-[#24B49A] hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium text-sm px-6 py-1.5 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800 md:mx-2  my-2">Sign
                         Up</a>
                 </li>
             </ul>
